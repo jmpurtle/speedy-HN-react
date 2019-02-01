b8bcb5baa9af05856b99607c70e342ed8220db66
John Purtle <purtlejohnmark@gmail.com>
2019-01-31 19:55:55 -0600

    MVP

--- src/App.js
+++ src/App.js
@@ -9,27 +9,36 @@ class App extends Component {
  
     this.state = {
       storyIds: [],
-      limit: 20,
-      offset: 0
+      limit: parseInt(this.getUrlParameter('limit', 20)),
+      offset: parseInt(this.getUrlParameter('offset', 0))
     }
     this.pageBack = this.pageBack.bind(this);
     this.pageForward = this.pageForward.bind(this);
     this.changeLimit = this.changeLimit.bind(this);
   }
 
-  pageBack() {
+  getUrlParameter(name, defaultVal) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(window.location.search);
+    return results === null ? defaultVal : decodeURIComponent(results[1].replace(/\+/g, ' '));
+  };
+
+  pageBack(e) {
+    e.preventDefault();
     const calculatedOffset = this.state.offset - this.state.limit;
     const newOffset = (calculatedOffset > 0) ? calculatedOffset : 0;
     this.setState({offset: newOffset});
   }
 
-  pageForward() {
+  pageForward(e) {
+    e.preventDefault();
     const newOffset = this.state.offset + this.state.limit;
     this.setState({offset: newOffset});
   }
 
   changeLimit(e) {
-    console.log(e.target.dataset.limit);
+    e.preventDefault();
     this.setState({limit: parseInt(e.target.dataset.limit)});
   }
 
@@ -46,23 +55,32 @@ class App extends Component {
     const lastSummary = limit + offset;
     const currentSummaries = storyIds.slice(offset, lastSummary);
 
+    const calculatedPageDownOffset = this.state.offset - this.state.limit;
+    const pageDownOffset = (calculatedPageDownOffset > 0) ? calculatedPageDownOffset : 0;
+
+    const pageUpOffset = this.state.offset + this.state.limit;
+
     const summaries = currentSummaries.map((item, key) => {
       return <HNSummary item={item} key={key} />;
     });
 
     return (
       <div className="App">
+        <header className="siteHeader">
+          <h1>Speedy Hacker News</h1>
+          <span>Less clicking, more reading</span>
+        </header>
         <div className="viewLimits">
-          <button onClick={this.changeLimit} data-limit="10">10 items</button>
-          <button onClick={this.changeLimit} data-limit="20">20 items</button>
-          <button onClick={this.changeLimit} data-limit="50">50 items</button>
+          <a className="btn limits" href={'/?limit=10&offset=' + this.state.offset} onClick={this.changeLimit} data-limit="10">10 items</a>
+          <a className="btn limits" href={'/?limit=20&offset=' + this.state.offset} onClick={this.changeLimit} data-limit="20">20 items</a>
+          <a className="btn limits" href={'/?limit=50&offset=' + this.state.offset} onClick={this.changeLimit} data-limit="50">50 items</a>
         </div>
         <ol>
           {summaries}
         </ol>
         <div className="pagination">
-          <button onClick={this.pageBack}>&lt;</button>
-          <button onClick={this.pageForward}>&gt;</button>
+          <a className="btn pageArrow" href={'/?limit=' + this.state.limit + '&offset=' + pageDownOffset} onClick={this.pageBack}>&lt;</a>
+          <a className="btn pageArrow" href={'/?limit=' + this.state.limit + '&offset=' + pageUpOffset} onClick={this.pageForward}>&gt;</a>
         </div>
       </div>
     );
